4dc41a0b7ff26313350a637715c6261f737a23e5
seaho5599 <seaho5599@gmail.com>
2022-06-24 16:39:19 +0900

    header수정,팝업창 닫기버튼 수정,반응형 추가

--- js/script.js
+++ js/script.js
@@ -1,4 +1,34 @@
-$(document).ready()
+$(document).ready(function(){
+  // 팝업닫기
+  let popup = $('.popup');
+  let popup_close = $('.popup-close');
+  let header = $('.header');
+  let header_top = $('.header-top');
+  popup_close.click(function(event){
+    // a 태그의 href 로 이동하는 기능 막기
+    event.preventDefault();
+    
+    popup.hide();
+  });
+  $(window).scroll(function(){
+    // 스크롤바의 위치를 파악한다.
+    let temp = $(window).scrollTop();
+    // .header-top 숨기기
+    if(temp > 0) {
+      header.addClass('header-fix');
+      header_top.addClass('popup-top-close');
+    }else{
+      header.removeClass('header-fix');
+      header_top.removeClass('popup-top-close');
+    }
+
+  });
+
+
+
+
+});
+
 window.onload = function () {
   new Swiper('.sw-event', {
     loop: true,
@@ -12,11 +42,11 @@ window.onload = function () {
     },
   });
 
-  $(document).mouseup(function (e){
-    if($(".popup-close").has(e.target).length === 0){
-      $(".popup").hide();
-    }
-  });
+  // $(document).mouseup(function (e){
+  //   if($(".popup-close").has(e.target).length === 0){
+  //     $(".popup").hide();
+  //   }
+  // });
 
 
 }
